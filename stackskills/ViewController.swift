000cc713aa8db07b9a5fb079054264376ebc7b30
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2017-10-23 21:13:59 +0800

    Done with string interpolation

--- stackskills/ViewController.swift
+++ stackskills/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: Any) {
       print(text1.text!)
         print(text2.text!)
-        
+        cooolLabel.text = "Answer is \(Double(text1.text!)! + Double(text2.text!)!)"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
