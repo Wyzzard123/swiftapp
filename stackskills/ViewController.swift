bb2762cdce0f2c8961e0fe706862c4ac57a36290
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2017-10-23 22:11:36 +0800

    improved the tutorial app with divide and stuff

--- stackskills/ViewController.swift
+++ stackskills/ViewController.swift
@@ -9,34 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var cooolLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
     
+
+    @IBOutlet weak var toggleAddSubtraction: UIButton!
+    var addition = true
+    var subtraction = false
+    var multiplication = false
+    var division = false
     
     @IBAction func makeitCool(_ sender: Any) {
-        cooolLabel.text = "Buttons are cool!"
+        if toggleAddSubtraction.titleLabel!.text! == "ADD" {
+            toggleAddSubtraction.setTitle("SUBTRACT", for: .normal)
+            toggleAddSubtraction.titleLabel!.text! = "SUBTRACT"
+             addition = false
+            subtraction = true
+        } else if toggleAddSubtraction.titleLabel!.text! == "SUBTRACT" {
+            toggleAddSubtraction.setTitle("MULTIPLY", for: .normal)
+             subtraction = false
+            multiplication = true
+            
+        } else if toggleAddSubtraction.titleLabel!.text! == "MULTIPLY" {
+            toggleAddSubtraction.setTitle("DIVIDE", for: .normal)
+             multiplication = false
+            division = true
+            
+        } else if toggleAddSubtraction.titleLabel!.text! == "DIVIDE" {
+            toggleAddSubtraction.setTitle("ADD", for: .normal)
+            division = false
+            addition = true
+        }
+        
+        
         print("Your mom")
     }
     @IBAction func buttonTapped(_ sender: Any) {
-      print(text1.text!)
-        print(text2.text!)
-        cooolLabel.text = "Answer is \(Double(text1.text!)! + Double(text2.text!)!)"
+        if addition == true {
+            cooolLabel.text = "Answer is \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else if subtraction == true {
+            cooolLabel.text = "Answer is \(Double(text1.text!)! - Double(text2.text!)!)"
+        } else if multiplication == true {
+            cooolLabel.text = "Answer is \(Double(text1.text!)! * Double(text2.text!)!)"
+        } else if division == true {
+            cooolLabel.text = "Answer is \(Double(text1.text!)! / Double(text2.text!)!)"
+        }
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
