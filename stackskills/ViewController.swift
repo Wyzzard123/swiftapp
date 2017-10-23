2587f0391a3676c5febaf013c64288444fd3b76f
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2017-10-23 10:55:53 +0800

    Initial Commit

---
+++ stackskills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackskills
+//
+//  Created by Wyz on 23/10/17.
+//  Copyright © 2017 Wyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
